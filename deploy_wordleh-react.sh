5d2d87f8c69739dd5219ebda3467bfbd764d8178
JS @breaktoprotect <breaktoprotect@gmail.com>
2022-02-03 12:58:23 +0800

    Bug fix

--- deploy_wordleh-react.sh
+++ deploy_wordleh-react.sh
@@ -1,6 +1,6 @@
 git pull
-cp ~/wordleh_engine.py /var/www/wordleh-flask
-cp ~/wordleh_app.py /var/www/wordleh-flask
-cd ~/wordleh-react
+cp ~/wordleh/wordleh_engine.py /var/www/wordleh-flask
+cp ~/wordleh/wordleh_app.py /var/www/wordleh-flask
+cd ~/wordleh/wordleh-react
 npm run build static
 sudo cp -r build/* /var/www/html
